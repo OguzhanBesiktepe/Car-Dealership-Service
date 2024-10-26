a538017125a04e6864d31a4f5b94a7669067dd8d
Himal <104921088+9bitbin@users.noreply.github.com>
2024-10-26 13:18:56 -0400

    -created the toggle button feature
    and kept the original button

---
+++ src/main/java/com/example/cardealershipservice/ThemeSwitchable.java
@@ -0,0 +1,7 @@
+package com.example.cardealershipservice;
+
+import java.awt.*;
+
+public interface ThemeSwitchable {
+    void applyTheme(Color backgroundColor); // Method to apply the theme change
+}
